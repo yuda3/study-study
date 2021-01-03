089d19bffdfdb476b4de099be8759e90e0e0df25
yuda3 <yuda3@hotmail.com>
2021-01-03 23:21:55 +0900

    Add PasswordForm & Validator

---
+++ src/main/java/appdev/appdev/settings/PasswordFormValidator.java
@@ -0,0 +1,21 @@
+package appdev.appdev.settings;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class PasswordFormValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return PasswordForm.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        PasswordForm passwordForm = (PasswordForm) target;
+
+        if(passwordForm.getNewPassword().equals( passwordForm.getNewPassword())){
+            errors.rejectValue("newPassword","wrong.value", "입력한 새 패스워드가 일치하지 않습니다.");
+        }
+
+    }
+}
--- src/main/java/appdev/appdev/settings/SettingsController.java
+++ src/main/java/appdev/appdev/settings/SettingsController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,15 +17,24 @@ import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
-
 public class SettingsController {
 
-    static final String SETTINGS_PROFILE_VIEW_NAME = "/settings/profile";
+    @InitBinder("password")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(new PasswordFormValidator());
+    }
+
+
+    static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
     static final String SETTINGS_PROFILE_URL = "/settings/profile";
+
+    static final String SETTINGS_PASSWORD_VIEW_NAME = "settings/password";
+    static final String SETTINGS_PASSWORD_URL = "/settings/password";
+
     private final AccountService accountService;
 
     @GetMapping(SETTINGS_PROFILE_URL)
-    public String profileUpdateForm(@CurrentUser Account account, Model model){
+    public String updateProfileForm(@CurrentUser Account account, Model model){
         model.addAttribute(account);
         model.addAttribute(new Profile(account));
         return SETTINGS_PROFILE_VIEW_NAME;
@@ -37,7 +48,28 @@ public class SettingsController {
             return SETTINGS_PROFILE_VIEW_NAME;
         }
         accountService.updateProfile(account, profile);
-        attributes.addFlashAttribute("message", "modify Completed");
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
         return "redirect:" + SETTINGS_PROFILE_URL;
+    }
+
+    @GetMapping(SETTINGS_PASSWORD_URL)
+    public String updatePasswordForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        model.addAttribute(new PasswordForm());
+        return SETTINGS_PASSWORD_VIEW_NAME;
+    }
+
+    @PostMapping(SETTINGS_PASSWORD_URL)
+    public String updatePassword(@CurrentUser Account account, @Valid PasswordForm passwordForm,
+                                 Errors errors,Model model, RedirectAttributes attributes){
+        if(errors.hasErrors()){
+            model.addAttribute(account);
+            return SETTINGS_PASSWORD_VIEW_NAME;
+        }
+
+        accountService.updatePassword(account, passwordForm.getNewPassword());
+        attributes.addFlashAttribute("message", "패스워드를 변경했습니다.");
+        return "redirect:"+ SETTINGS_PROFILE_URL;
+
     }
 }
