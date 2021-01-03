6c8a1f6d4aac244479d5226dfb4089b0c2750e74
yuda3 <yuda3@hotmail.com>
2021-01-03 14:42:21 +0900

    modify Controller & SecurityConfig & Test Add view & SignUpForm

---
+++ src/main/java/appdev/appdev/account/SignUpForm.java
@@ -0,0 +1,12 @@
+package appdev.appdev.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+    private String email;
+    private String password;
+
+}
