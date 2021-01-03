2c421e3033fc3db7c2563066b25db6dd4629bf62
yuda3 <yuda3@hotmail.com>
2021-01-03 14:56:55 +0900

    modify Controller Add SignUpFormValidator & AccountRepository

---
+++ src/main/java/appdev/appdev/account/SignUpFormValidator.java
@@ -0,0 +1,29 @@
+package appdev.appdev.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SignUpForm signUpForm = (SignUpForm) errors;
+        if(accountRepository.existsByEmail(signUpForm.getEmail())){
+            errors.rejectValue("email", "invalid.email", new Object[]{signUpForm.getEmail()}, "Please Check email Address ");
+        }
+        if(accountRepository.existsByNickname(signUpForm.getEmail())){
+            errors.rejectValue("nickname", "invalid.nickname", new Object[]{signUpForm.getNickname()}, "Please Check Nickname");
+        }
+    }
+}
