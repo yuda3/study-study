6c9cf3fc835629e29cc6c171d8dea7823ee20447
yuda3 <yuda3@hotmail.com>
2021-01-03 22:02:44 +0900

    add profile and had bug

--- src/main/java/appdev/appdev/account/AccountController.java
+++ src/main/java/appdev/appdev/account/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -76,6 +77,17 @@ public class AccountController {
         }
         accountService.sendSignUpConfirmEmail(account);
         return "redirect:/";
+    }
+
+    @GetMapping("/profile/{nickname}")
+    public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser Account  account){
+        Account byNickname = accountRepository.findByNickname(nickname);
+        if(nickname == null){
+            throw new IllegalArgumentException("해당하는 사용자가 없습니다.");
+        }
+        model.addAttribute(byNickname);
+        model.addAttribute("isOwner", byNickname.equals(account));
+        return "account/profile";
     }
 
 }
